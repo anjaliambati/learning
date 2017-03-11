4789eb70e5686156cedad3c69b3d453bcf1feaf7
anjaliambati <anjaliambati@protonmail.com>
2017-03-11 14:40:43 -0800

    singing computer

---
+++ singing.py
@@ -0,0 +1,3 @@
+ import os
+ os.system("say -v 'Good News' this land is your land, this land is my land, from california to new york island, from redwood forest to golden waters, this land is made for you and me")
+ 
